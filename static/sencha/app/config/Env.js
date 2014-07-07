ae2bb27ee6ecfe88ba6ba6b0e3e58cc084aa3983
Seth Chase <blinddiver@gmail.com>
2014-07-07 13:21:30 -0400

    commented out csrf token function

--- static/sencha/app/config/Env.js
+++ static/sencha/app/config/Env.js
@@ -5,12 +5,12 @@ Ext.define("TransportApp.config.Env", {
     }
 });
 
-Ext.Ajax.on('beforerequest', function (conn, options) {
-   if (!(/^http:.*/.test(options.url) || /^https:.*/.test(options.url))) {
-     if (typeof(options.headers) == "undefined") {
-       options.headers = {'X-CSRFToken': Ext.util.Cookies.get('csrftoken')};
-     } else {
-       options.headers.extend({'X-CSRFToken': Ext.util.Cookies.get('csrftoken')});
-     }
-   }
-}, this);
+//Ext.Ajax.on('beforerequest', function (conn, options) {
+//   if (!(/^http:.*/.test(options.url) || /^https:.*/.test(options.url))) {
+//     if (typeof(options.headers) == "undefined") {
+//       options.headers = {'X-CSRFToken': Ext.util.Cookies.get('csrftoken')};
+//     } else {
+//       options.headers.extend({'X-CSRFToken': Ext.util.Cookies.get('csrftoken')});
+//     }
+//   }
+//}, this);
