7a0d70183625e4476fedb0070df06e21c5166ac4
Robert Dougan <rdougan@me.com>
2014-07-28 22:53:33 +0200

    Adding plugins to android

--- static/sencha/phonegap/platforms/android/ant-build/Aporo-debug-unaligned.apk.d
+++ static/sencha/phonegap/platforms/android/ant-build/Aporo-debug-unaligned.apk.d
@@ -2,3 +2,4 @@
 /Users/rdougan/Development/odesk/23-07-2014/Seth Chase/aporo/static/sencha/phonegap/platforms/android/ant-build/Aporo.ap_ \
 /Users/rdougan/Development/odesk/23-07-2014/Seth Chase/aporo/static/sencha/phonegap/platforms/android/ant-build/classes.dex \
 /Users/rdougan/Development/odesk/23-07-2014/Seth Chase/aporo/static/sencha/phonegap/platforms/android/CordovaLib/ant-build/classes.jar \
+/Users/rdougan/Development/odesk/23-07-2014/Seth Chase/aporo/static/sencha/phonegap/platforms/android/libs/com.google.zxing.client.android.captureactivity.jar \
