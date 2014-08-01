43559fe000773f66ebcce10a5eaa854969f8b0e2
Robert Dougan <rdougan@me.com>
2014-08-01 23:58:33 +0200

    Fixing extjs xmin app

--- static/xmin/app.js
+++ static/xmin/app.js
@@ -1,15 +1,20 @@
-Ext.BLANK_IMAGE_URL = '/static/xmin/images/s.gif';
-Ext.tip.QuickTipManager.init();
-Ext.Loader.setConfig({enabled:true});
-Ext.Loader.setPath('Ext.ux', '/static/extjs-4/examples/ux');
-Ext.Ajax.defaultHeaders = {
-        'X-CSRFToken': Ext.util.Cookies.get('csrftoken')
+gettext = function(text) {
+    return text;
 };
+
+Ext.BLANK_IMAGE_URL = '/static/xmin/images/s.gif';
+
+Ext.Loader.setConfig({
+    enabled: true
+});
+
+Ext.Loader.setPath('Ext', '../extjs-4/src');
+Ext.Loader.setPath('Ext.ux', '../extjs-4/examples/ux');
 
 Ext.application({
     name: 'Xmin',
 
-    appFolder: '/static/xmin',
+    appFolder: '../xmin',
 
     controllers: [
         'Dashboard',
@@ -18,7 +23,19 @@ Ext.application({
         'Admin'
     ],
 
+    requires: [
+        'Ext.util.History',
+        'Ext.util.Cookies',
+        'Ext.window.MessageBox'
+    ],
+
     launch: function() {
+        Ext.tip.QuickTipManager.init();
+
+        Ext.Ajax.defaultHeaders = {
+            'X-CSRFToken': Ext.util.Cookies.get('csrftoken')
+        };
+
         Xmin.application = this;
 
         Ext.create('Ext.container.Viewport', {
--- static/xmin/controller/Router.js
+++ static/xmin/controller/Router.js
@@ -1,67 +1,63 @@
 Ext.define('Xmin.controller.Router', {
     extend: 'Ext.app.Controller',
 
-    requires  : [
-    ],
+    requires: [],
 
-    views: [
-    ],
+    views: [],
 
-    stores: [
-    ],
+    stores: [],
 
-    models: [
-    ],
+    models: [],
 
-    refs: [
-    ],
+    refs: [],
 
-    urls:[],
+    urls: [],
 
     init: function() {
         Ext.util.History.init();
         Ext.util.History.on('change', this.onNavigate, this);
 
-        this.registerUrls([
-            { pattern: '^/test/$', callback: this.log }
-        ]);
+        this.registerUrls([{
+            pattern: '^/test/$',
+            callback: this.log
+        }]);
     },
 
-    log: function(){
+    log: function() {
         console.log(arguments);
     },
 
-    navigate: function(token){
+    navigate: function(token) {
         Ext.util.History.add(token);
     },
 
-    onNavigate: function(token){
+    onNavigate: function(token) {
         console.log('react to token:', token);
 
-        Ext.Array.forEach(this.urls, function(item, index, allItems){
-            if(item.regexp.test(token)){
+        Ext.Array.forEach(this.urls, function(item, index, allItems) {
+            if (item.regexp.test(token)) {
                 console.log(item.pattern, 'matched token', token);
                 item.callback(token);
             }
         });
     },
 
-    registerUrls: function(urls){
+    registerUrls: function(urls) {
         var that = this,
             active_token = Ext.util.History.getHash(),
             active_token_matched = false;
 
 
-        Ext.Array.forEach(urls, function(item, index, allItems){
+        Ext.Array.forEach(urls, function(item, index, allItems) {
             item.regexp = RegExp(item.pattern);
             that.urls.push(item)
 
-            if(item.regexp.test(active_token)){
+            if (item.regexp.test(active_token)) {
                 active_token_matched = true;
             }
         });
 
-        if(active_token_matched){
+        if (active_token_matched) {
             this.onNavigate(active_token);
         }
     }
